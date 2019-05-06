0a4b096be472cd7720aecafa90aa1ea0dac1622e
Parallelix <dario.fabio.sala@gmail.com>
2019-05-06 16:05:41 +0200

    createSubjectGroup, addGroup, updateGrades methods created

--- scripts/script.js
+++ scripts/script.js
@@ -14,56 +14,70 @@ let s4 = "java";
 let ws4 = 0.25;
 let g4 = 5;
 let s5 = "csharp";
-let ws5 = 0.5;
+let ws5 = 0.25;
 let g5 = 6;
 let s6 = "haskel";
 let ws6 = 0.25;
 let g6 = 5;
+let s7 = "python";
+let ws7 = 0.25;
+let g7 = 6;
 
 let groupId = 0;
-
-// subject contains s = name, ws = weight, g = grade
 let subject = {groupId, s1, ws1, g1};
 
-/*
-// probably has to be a map with key=name / value=subjectarray
-// subjectGroup containing an array of subjectgroups, containing several subjects, the sum of their weight has to be 1 (100%)
- let subjectGroups = [[], [],[]];
-
-let createSubject = function(myGroupId = groupId, name = "no Name passed", weight = 0.1, grade = 1) {
-    if(myGroupId === 0 && subjectGroups === null){
-        subjectGroups[0] = [{myGroupId, name, weight, grade}]
-    }
-    else if(myGroupId == 0){
-        subjectGroups[0].push({myGroupId, name, weight, grade});
-    }
-    else{
-        subjectGroups[1].push({myGroupId, name, weight, grade});
-    }
-    //  subjectGroups.push( {myGroupId, name, weight, grade});
-}
-*/
-// let subjectGroups = new Map([[0, []], [1, []], [2, []] ]);
+// map mit key=groupid / value=subject[]
 let subjectGroups = new Map();
+// map mit key=groupid / value=(float)wantedGrade
+let groupsWantedGrade = new Map();
 
-let createSubject = function(myGroupId = groupId, name = "no Name passed", weight = 0.1, grade = 1) {
-    //for ( var k of subjectGroups.key()){
-    console.log(groupId);
+
+let createSubject = function(myGroupId = groupId, nameOfSubject = "no Name passed", weight = 0.1, grade = -1) {
     for (let [k, v] of subjectGroups) {
         if(k === myGroupId){
             if( Array.isArray(v)){
-                v.push({myGroupId, name, weight, grade})
+                v.push({myGroupId, nameOfSubject, weight, grade})
             }
         }
     }
 }
 
+const persons = [{ "name":"A", "salary":1200 }, { "name":"B", "salary": 1500 }];
+
+persons.forEach(item => item.salary += 1000);
+
+console.log(persons)
+
+
 let addGroup = function(){
-    //console.log(groupId);
     subjectGroups.set(groupId, []);
+    groupsWantedGrade.set(groupId, 4)
     groupId ++;
 }
 
+
+
+// v = array[object, object, ...]
+let updateGrades = function(subjectName, grade) {
+    for (let [key, value] of subjectGroups) {
+        console.log("Key: " + key);
+        // console.log("Value: " + value);
+        for (let i = 0; i < value.length; i++) {
+            console.log("Value: " + value[i].nameOfSubject);
+
+            if (value[i].nameOfSubject === subjectName) {
+                console.log("Match: " + value[i].nameOfSubject);
+                value[i].grade = grade;
+            }
+        }
+    }
+}
+
+let calculateGrades = function(subjectGroupID, desiredGrade){
+
+}
+
+// Test-data:
 addGroup()
 createSubject(0, s1, ws1, g1);
 createSubject(0, s2, ws2, g2);
@@ -72,39 +86,10 @@ addGroup();
 createSubject(1, s4, ws4, g4);
 createSubject(1, s5, ws5, g5);
 createSubject(1, s6, ws6, g6);
+createSubject(1, s7, ws7, g7);
 
+updateGrades(s1, 6)
 console.log(subjectGroups);
 
-/*
-for (let [k, v] of subjectGroups) {
-    {console.log(k);}
-    {console.log(v);}
-}*/
-
-/*
-let gradesAndWeights = new Map();
-
-gradesAndWeights.set(s1, ws1);
-gradesAndWeights.set(s2, ws2);
-gradesAndWeights.set(s3, ws3);
-
-let subjectGropus = new Map();
-
-gradesAndWeights.set();
-gradesAndWeights.get();
-gradesAndWeights.size;
 
 
-for (var key of gradesAndWeights.keys()) {
-    console.log(key);
-}
-for (var value of gradesAndWeights.values()) {
-    console.log(value);
-}
-for (var [key, value] of gradesAndWeights.entries()) {
-    console.log(key + ' = ' + value);
-}
-
-let subjectGroupes = [];
-
-*/
